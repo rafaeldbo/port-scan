921ad0b23c22a3b3dd5d8e287cb7354e658d7c6f
rafaeldbo <rafaeldbo@al.insper.edu.br>
2025-05-04 20:56:05 -0400

    alterando nome do modulo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'portscan=app.main:main',
+            'portscan=portscan.main:main',
         ],
     },
 )
